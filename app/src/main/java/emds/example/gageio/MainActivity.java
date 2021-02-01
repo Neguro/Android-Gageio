755f836f7aa721044480bb805c42e2bac8137b2d
Neguro <nassim.ahmedali@gmail.com>
2021-02-01 15:42:45 +0100

    gage.io v1 : premier commit du dépot

---
+++ app/src/main/java/emds/example/gageio/MainActivity.java
@@ -0,0 +1,50 @@
+package emds.example.gageio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.transition.Transition;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Button;
+import java.io.*;
+import java.net.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnCreePartie;
+    private Button btnRejoindrePartie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCreePartie=(Button)findViewById(R.id.btnCreePartie);
+        btnRejoindrePartie=(Button)findViewById(R.id.btnRejoindrePartie);
+
+        btnRejoindrePartie.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenRejoindreGroupe();
+            }
+        }));
+
+        btnCreePartie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  OpenCreeGrooupe();
+            }
+        });
+    }
+    public void OpenCreeGrooupe(){
+        Intent intent=new Intent(this,CreerGroupeActivity.class); //Fonction qui permet d'ouvrir une autre activité
+        startActivity(intent);
+    }
+
+    public  void OpenRejoindreGroupe(){
+        Intent intent=new Intent(this,RejoindreGroupeActivity.class); //Fonction qui permet d'ouvrir une autre activité
+        startActivity(intent);
+    }
+}
